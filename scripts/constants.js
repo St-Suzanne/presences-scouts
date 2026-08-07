f670f31eaaec7e040531ecfe66a2cbe06a862198
LarsHanquet <145200507+LarsHanquet@users.noreply.github.com>
2026-08-07 10:42:10 +0200

    created constant.js to handle all constants

---
+++ scripts/constants.js
@@ -0,0 +1,13 @@
+export const API_URL = "https://script.google.com/macros/s/AKfycbyJ7HSzYbKpq7NEjJ5LZQqtRVUMQ7x1fQeM-R97vzWAhk0M4oM-1t8Xd01yoZDjRO6h/exec";
+
+export const PRESENCE_STATUS = {
+  PRESENT: "Present",
+  JUSTIFIED_ABSENCE: "Absence justifiee",
+  UNJUSTIFIED_ABSENCE: "Absence non justifiee"
+};
+
+export const PRESENCE_LABELS = {
+  [PRESENCE_STATUS.PRESENT]: "Présent",
+  [PRESENCE_STATUS.JUSTIFIED_ABSENCE]: "Absence justifiée",
+  [PRESENCE_STATUS.UNJUSTIFIED_ABSENCE]: "Absence non justifiée"
+};
